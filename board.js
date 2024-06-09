0c4883d10bec0989a7f7cd3030dd7f2a77d0ad58
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-09 23:03:45 +0300

    Add fiftyMove and side properties to GameBoard object

--- board.js
+++ board.js
@@ -1,3 +1,7 @@
-let gamBoard = {}
+import { COLOURS } from "./defs";
 
-gamBoard.pieces = new Array(BRD_SQ_NUM);
+let GameBoard = {}
+
+GameBoard.pieces = new Array(BRD_SQ_NUM);
+GameBoard.side= COLOURS.WHITE;
+GameBoard.fiftyMove = 0;  // if fifty moves were made by either sides with no captures or pawn movement, the game is drawn
