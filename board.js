861d22b85ec6e732c12daba48c23642ff8063eaf
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-09 23:57:39 +0300

    Add ply and hisPly to GameBoard object

--- board.js
+++ board.js
@@ -5,3 +5,6 @@ let GameBoard = {}
 GameBoard.pieces = new Array(BRD_SQ_NUM);
 GameBoard.side= COLOURS.WHITE;
 GameBoard.fiftyMove = 0;  // if fifty moves were made by either sides with no captures or pawn movement, the game is drawn
+
+GameBoard.ply = 0; // a ply is half of a move (1move = 2 plies). ply here means depth of the search tree.
+GameBoard.hisPly = 0;  // history ply is the number of moves/2
