cc3211f865867f8be4045618e068d7cc997c343e
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-09 23:59:02 +0300

    Add castlePerm to GameBoard object

--- board.js
+++ board.js
@@ -8,3 +8,7 @@ GameBoard.fiftyMove = 0;  // if fifty moves were made by either sides with no ca
 
 GameBoard.ply = 0; // a ply is half of a move (1move = 2 plies). ply here means depth of the search tree.
 GameBoard.hisPly = 0;  // history ply is the number of moves/2
+
+GameBoard.castlePerm= 0;  //decimal number(0-15) represents the permitability of each side to castle on a side
+                        // for ex: 1101: white can castle on kings side, cant castle on queen's side
+                        // black can castle on kings side, can castle on queen's side
