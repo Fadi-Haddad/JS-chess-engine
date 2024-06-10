6cded34c65dea24959a953bb94885a5a5a7744b0
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-10 23:32:26 +0300

    Add material array to GameBoard object to hold metrial of each player

--- board.js
+++ board.js
@@ -13,3 +13,5 @@ GameBoard.castlePerm= 0;  //decimal number(0-15) represents the permitability of
              // for ex: 1101: white can castle on kings side, can't castle on queen's side
             // black can castle on kings side, can castle on queen's side.
             // to fine out if white can castle on kings side, we do a bitwise & between castlePerm and WKCA value in CASTLEBIT object
+
+GameBoard.material = new Array(2); // White, Black material of pieces
