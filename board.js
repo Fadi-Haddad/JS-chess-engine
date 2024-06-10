4e5c8c87fc5bff622112fad04de15d56bfe7748a
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-10 22:26:41 +0300

    Add different arrays to describe pieces

--- board.js
+++ board.js
@@ -10,5 +10,6 @@ GameBoard.ply = 0; // a ply is half of a move (1move = 2 plies). ply here means 
 GameBoard.hisPly = 0;  // history ply is the number of moves/2
 
 GameBoard.castlePerm= 0;  //decimal number(0-15) represents the permitability of each side to castle on a side
-                        // for ex: 1101: white can castle on kings side, cant castle on queen's side
-                        // black can castle on kings side, can castle on queen's side
+             // for ex: 1101: white can castle on kings side, can't castle on queen's side
+            // black can castle on kings side, can castle on queen's side.
+            // to fine out if white can castle on kings side, we do a bitwise & between castlePerm and WKCA value in CASTLEBIT object
