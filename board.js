4883b65eec004908247b1636bc6e81053b49fb5c
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2024-06-11 13:30:32 +0300

    Add enPas property to the GameBoard object

--- board.js
+++ board.js
@@ -4,6 +4,7 @@ let GameBoard = {}
 
 GameBoard.pieces = new Array(BRD_SQ_NUM);
 GameBoard.side= COLOURS.WHITE;
+GameBoard.enPas= 0;
 GameBoard.fiftyMove = 0;  // if fifty moves were made by either sides with no captures or pawn movement, the game is drawn
 
 GameBoard.ply = 0; // a ply is half of a move (1move = 2 plies). ply here means depth of the search tree.
